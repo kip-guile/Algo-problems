bb65d25cf161c9944b7226acdb8dd194e182e769
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-08-11 19:52:38 +0100

    sort in place header

--- code/easy.js
+++ code/easy.js
@@ -50,6 +50,8 @@ Given a sorted array nums, remove the duplicates in-place such that each element
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 */
 
+//SORT IN PLACE
+
 var removeDuplicates = function (nums) {
   let slow_index = 0
   for (let i = 0; i < nums.length; i++) {

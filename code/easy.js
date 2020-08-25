1421d75956aff842fc700e371d0548f53ff10c4f
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-08-25 22:57:33 +0100

    house robber

--- code/easy.js
+++ code/easy.js
@@ -228,3 +228,30 @@ var duplicateZeros = function (arr) {
   }
   console.log(arr)
 }
+
+// You are a professional robber planning to rob houses along a street.Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+// Given a list of non - negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+//   Example 1:
+
+// Input: nums = [1, 2, 3, 1]
+// Output: 4
+// Explanation: Rob house 1(money = 1) and then rob house 3(money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+
+var rob = function (nums) {
+  let memo = {}
+  function choose(nums, i) {
+    if (i < 0) {
+      return 0
+    }
+    if (memo[i] !== undefined) {
+      return memo[i]
+    }
+    let ans = Math.max(nums[i] + choose(nums, i - 2), choose(nums, i - 1))
+    memo[i] = ans
+    return ans
+  }
+  return choose(nums, nums.length - 1)
+}
